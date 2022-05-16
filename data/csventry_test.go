d80abab33c03122384c588d83ece1fa64569d647
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2022-05-16 17:44:03 +0100

    adding tests

--- data/csventry_test.go
+++ data/csventry_test.go
@@ -22,3 +22,66 @@ func TestSortDomainsAlphabetically(t *testing.T) {
 		}
 	}
 }
+
+func TestSortDomainsAlphabetically_WhenNameIsSameButAddressIsDifferent(t *testing.T) {
+	recordInfo := map[string]int{
+		"bing.com":   7,
+		"bing.ie":    2,
+		"bing.co.uk": 1,
+	}
+	expectedOutput := []string{"bing.co.uk", "bing.com", "bing.ie"}
+
+	output := SortDomainsAlphabetically(recordInfo)
+
+	for i, v := range output {
+		if v != expectedOutput[i] {
+			t.Errorf("TestSortDomainsAlphabetically Failed!")
+		} else {
+			t.Logf("TestSortDomainsAlphabetically Passed!")
+		}
+	}
+}
+
+func TestRetreiveDomainName(t *testing.T) {
+	fullEmailAddress := "www.legolas@gmail.com"
+
+	output, err := RetrieveDomainName(fullEmailAddress)
+
+	if err != nil {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else if output != "gmail.com" {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else {
+		t.Logf("TestRetreiveDomainName Passed!")
+	}
+}
+
+func TestRetreiveDomainName_ShouldOnlyRetrieveTextAfterTheFinalAtSymbol(t *testing.T) {
+	fullEmailAddress := "www.legolas@gmail.com@yahoo.ie"
+
+	output, err := RetrieveDomainName(fullEmailAddress)
+
+	if err != nil {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else if output != "yahoo.ie" {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else {
+		t.Logf("TestRetreiveDomainName Passed!")
+	}
+}
+
+func TestRetreiveDomainName_ShouldReturnErrorWhenEmailIsInvalid(t *testing.T) {
+	fullEmailAddress := "email"
+
+	output, err := RetrieveDomainName(fullEmailAddress)
+
+	if output != "" {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else if err == nil {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else if err.Error() != "invalid domain name" {
+		t.Errorf("TestRetreiveDomainName Failed!")
+	} else {
+		t.Logf("TestRetreiveDomainName Passed!")
+	}
+}
